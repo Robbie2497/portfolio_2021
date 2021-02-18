b1f264adafa2d01d13e645bc7e611b015db56dd9
Robert McGrew <robbie2497@gmail.com>
2021-02-17 23:11:26 -0700

    uploading portfolio files

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
 });
 
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vpzzt.mongodb.net/<dbname>?retryWrites=true&w=majority`), 
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, 
 { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }
 );
 
